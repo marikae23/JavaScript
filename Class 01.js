da7bba5b1cbedcbb679e4dfa99b22b977c11567b
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-01 12:12:16 -0300

    Update Class 01.js

--- Class 01.js
+++ Class 01.js
@@ -2,3 +2,13 @@
 
 // This line prints a message to the console
 console.log("I am starting to learn JavaScript!");
+
+// Declare two variables to store the numbers.
+let num1 = 5;
+let num2 = 10;
+
+// Calculate the sum of num1 and num2 and store it in a variable.
+let sum = num1 + num2;
+
+// Display the result in the console.
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
